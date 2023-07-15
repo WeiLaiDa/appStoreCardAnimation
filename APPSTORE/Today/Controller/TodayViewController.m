f5919bd3cfa037b280ca8eb796657dec9d65a39e
许一鸣 <332214840@qq.com>
2023-07-15 13:26:37 +0800

    update 1.更新

--- APPSTORE/Today/Controller/TodayViewController.m
+++ APPSTORE/Today/Controller/TodayViewController.m
@@ -66,6 +66,9 @@ static NSString * const TodayTableVIewCellID = @"TodayTableVIewCell";
     if (!cell) {
         return;
     }
+    if (!cell) {
+        return;
+    }
     self.selectedCell = cell;
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
